d24f96f3c6956134089ea01d01db03273ef9039b
Xu Jack <110009224+xack1211@users.noreply.github.com>
2025-12-05 22:55:51 +0800

    Update

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './index.jsx';
+
+// Mount the React app to the root element
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
